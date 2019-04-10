4b1884b03494ebf57051e35461bdfb3492fac760
Ethan <ethan@venuenext>
2019-04-09 20:56:13 -0700

    bankwest_api/scorpio

---
+++ lib/bankwest_api/scorpio.rb
@@ -0,0 +1,11 @@
+require 'scorpio'
+require 'bankwest_api'
+
+module BankwestAPI
+  Document = Scorpio::OpenAPI::V3::Document.new(::YAML.load(BANKWEST_OPENAPI3_YML))
+
+  # GENERATED (relying on activesupport) vaguely like
+  # puts BankwestAPI::Document.components.schemas.select { |k,v| ['object', nil].include?(v['type']) }.keys.map { |k| "#{k.camelize} = ... '#{k}'" }
+
+  Errors = JSI.class_for_schema(Document.components.schemas['errors'])
+end
