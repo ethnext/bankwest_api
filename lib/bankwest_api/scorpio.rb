8a8bf83e02cefaae755c3f0b46f973ec2e791565
Ethan <ethan@venuenext>
2019-04-13 15:23:59 -0700

    token schema

--- lib/bankwest_api/scorpio.rb
+++ lib/bankwest_api/scorpio.rb
@@ -8,4 +8,5 @@ module BankwestAPI
   # puts BankwestAPI::Document.components.schemas.select { |k,v| ['object', nil].include?(v['type']) }.keys.map { |k| "#{k.camelize} = ... '#{k}'" }
   Errors        = JSI.class_for_schema(Document.components.schemas['errors'])
   TransactionPut = JSI.class_for_schema(Document.components.schemas['transactionPut'])
+  Token         = JSI.class_for_schema(Document.components.schemas['token'])
 end
